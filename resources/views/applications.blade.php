8dc2613cf922fd00cc0955e00a149d818283525f
chen <chenkuokloong@tamco.com.my>
2021-05-23 15:49:01 +0800

    interface little modify

--- resources/views/applications.blade.php
+++ resources/views/applications.blade.php
@@ -1,31 +1,43 @@
 <div class="antialiased container mx-auto flex flex-wrap bg-gray-100 pb-4 pt-5 h-screen sm:h-auto overflow-y-auto overflow-x-hidden sm:overflow-hidden">
 
-    <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+    <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fas fa-book-open">
+        <x-slot name="app_name">Library System</x-slot>
     </x-apps>
 
     <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+        <x-slot name="app_name">App 1</x-slot>
     </x-apps>
 
     <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+        <x-slot name="app_name">App 2</x-slot>
     </x-apps>
 
     <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+        <x-slot name="app_name">App 3</x-slot>
     </x-apps>
 
     <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+        <x-slot name="app_name">App 4</x-slot>
     </x-apps>
 
     <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+        <x-slot name="app_name">App 5</x-slot>
     </x-apps>
 
     <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
-        <x-slot name="app_name">About Me</x-slot>
+        <x-slot name="app_name">App 6</x-slot>
+    </x-apps>
+
+    <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
+        <x-slot name="app_name">App 7</x-slot>
+    </x-apps>
+    
+    <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
+        <x-slot name="app_name">App 8</x-slot>
+    </x-apps>
+    
+    <x-apps link="{{ route('personal:portfolio.index') }}" app-icon="fa fa-user">
+        <x-slot name="app_name">App 9</x-slot>
     </x-apps>
 
 </div>
